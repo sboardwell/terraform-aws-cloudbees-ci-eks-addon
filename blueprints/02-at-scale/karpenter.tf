ecf76c00b05dcb5f8fcbba5bf802e9942e76c557
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2025-05-12 09:31:04 +0200

    doc: removing duplications on karpenter behaviour

--- blueprints/02-at-scale/karpenter.tf
+++ blueprints/02-at-scale/karpenter.tf
@@ -198,7 +198,6 @@ spec:
         - key: kubernetes.io/os
           operator: In
           values: ["windows"]
-        # Karpenter supposed to use Spot instances first (because they are cheaper), switching to On-demand only if there are no Spot instances available
         - key: karpenter.sh/capacity-type
           operator: In
           values: ["spot", "on-demand"]
@@ -305,7 +304,6 @@ spec:
         - key: kubernetes.io/os
           operator: In
           values: ["windows"]
-        # Karpenter supposed to use Spot instances first, switching to On-demand only if there are no Spot instances available
         - key: karpenter.sh/capacity-type
           operator: In
           values: ["spot", "on-demand"]
