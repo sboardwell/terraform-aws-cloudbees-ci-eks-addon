8e37a21e40b3f38d362b9d165fe0e2f99a4c553d
sboardwell <sboardwell@cloudbees.com>
2025-09-23 09:06:32 +0200

    Do not pass root all the time

--- blueprints/helpers.sh
+++ blueprints/helpers.sh
@@ -65,11 +65,12 @@ bpAgent-dRun (){
 
 deploy () {
   terraform -chdir="$SCRIPTDIR/$ROOT" init
-  terraform -chdir="$SCRIPTDIR/$ROOT" plan -no-color > "$SCRIPTDIR/$ROOT/tfplan.txt"
+  terraform -chdir="$SCRIPTDIR/$ROOT" plan -out "$SCRIPTDIR/$ROOT/tfplan"
+  terraform -chdir="$SCRIPTDIR/$ROOT" show "$SCRIPTDIR/$ROOT/tfplan" -no-color > "$SCRIPTDIR/$ROOT/tfplan.txt"
   if [ "$CI" == "false" ]; then
     ask-confirmation "Deploy $ROOT. Check plan at $ROOT/tfplan.txt" || exit 0
   fi
-  tf-apply "$ROOT"
+  tf-apply
   INFO "CloudBees CI Blueprint $ROOT Deploy target finished succesfully."
 }
 
@@ -82,7 +83,7 @@ validate () {
     fi
     ask-confirmation "$msg" || exit 0
   fi
-  probes "$ROOT"
+  probes
   INFO "CloudBees CI Blueprint $ROOT Validation target finished succesfully."
 }
 
@@ -91,9 +92,9 @@ destroy () {
     ask-confirmation "Destroy $ROOT with Destroy Workloads Only=$DESTROY_WL_ONLY" || exit 0
   fi
   if [ "$DESTROY_WL_ONLY" == "false" ]; then
-    tf-destroy "$ROOT"
+    tf-destroy
   else
-    tf-destroy-wl "$ROOT"
+    tf-destroy-wl
   fi
   INFO "CloudBees CI Blueprint $ROOT Destroy target finished succesfully. Destroy Workloads Only=$DESTROY_WL_ONLY"
 }
@@ -136,118 +137,112 @@ retry () {
 }
 
 tf-output () {
-  local root="$1"
-  local output="$2"
-  terraform -chdir="$SCRIPTDIR/$root" output -raw "$output" 2> /dev/null
+  local output="$1"
+  terraform -chdir="$SCRIPTDIR/$ROOT" output -raw "$output" 2> /dev/null
 }
 
 #https://aws-ia.github.io/terraform-aws-eks-blueprints/getting-started/#deploy
 tf-apply () {
-  local root="$1"
-  export TF_LOG_PATH="$SCRIPTDIR/$root/terraform.log"
-  PLANFILE="$SCRIPTDIR/$ROOT/tfplan.txt"
+  export TF_LOG_PATH="$SCRIPTDIR/$ROOT/terraform.log"
   rm "$TF_LOG_PATH" 2> /dev/null || INFO "No previous log found."
-  retry 3 "terraform -chdir=$SCRIPTDIR/$root apply -target=module.vpc -auto-approve $PLANFILE"
+  retry 3 "terraform -chdir=$SCRIPTDIR/$ROOT apply -target=module.vpc -auto-approve $SCRIPTDIR/$ROOT/tfplan"
   INFO "Apply target module.vpc completed."
-  retry 3 "terraform -chdir=$SCRIPTDIR/$root apply -target=module.eks -auto-approve $PLANFILE"
+  retry 3 "terraform -chdir=$SCRIPTDIR/$ROOT apply -target=module.eks -auto-approve $SCRIPTDIR/$ROOT/tfplan"
   INFO "Apply target module.eks completed."
-  retry 3 "terraform -chdir=$SCRIPTDIR/$root apply -auto-approve $PLANFILE"
+  retry 3 "terraform -chdir=$SCRIPTDIR/$ROOT apply -auto-approve $SCRIPTDIR/$ROOT/tfplan"
   INFO "Apply the rest completed."
-  terraform -chdir="$SCRIPTDIR/$root" output > "$SCRIPTDIR/$root/terraform.output"
+  terraform -chdir="$SCRIPTDIR/$ROOT" output > "$SCRIPTDIR/$ROOT/terraform.output"
   INFO "Outputs saved corretely."
 }
 
 #https://aws-ia.github.io/terraform-aws-eks-blueprints/getting-started/#destroy
 tf-destroy () {
-  local root="$1"
-  export TF_LOG_PATH="$SCRIPTDIR/$root/terraform.log"
+  export TF_LOG_PATH="$SCRIPTDIR/$ROOT/terraform.log"
   rm "$TF_LOG_PATH" 2> /dev/null || INFO "No previous log found."
-  tf-destroy-wl "$root"
-  retry 3 "terraform -chdir=$SCRIPTDIR/$root destroy -target=module.eks -auto-approve"
+  tf-destroy-wl
+  retry 3 "terraform -chdir=$SCRIPTDIR/$ROOT destroy -target=module.eks -auto-approve"
   INFO "Destroy target module.eks completed."
   #Prevent Issue #165
   #TODO: Run only when terraform output is present
-  if [ "$root" == "${BLUEPRINTS[1]}" ]; then
-    aws_region=$(tf-output "$root" aws_region)
-    eks_cluster_name=$(tf-output "$root" eks_cluster_name)
-    bash "$SCRIPTDIR/$root/k8s/kube-prom-destroy.sh" "$eks_cluster_name" "$aws_region"
+  if [ "$ROOT" == "${BLUEPRINTS[1]}" ]; then
+    aws_region=$(tf-output "$ROOT" aws_region)
+    eks_cluster_name=$(tf-output "$ROOT" eks_cluster_name)
+    bash "$SCRIPTDIR/$ROOT/k8s/kube-prom-destroy.sh" "$eks_cluster_name" "$aws_region"
     INFO "kube-prom-destroy.sh completed."
   fi
-  retry 3 "terraform -chdir=$SCRIPTDIR/$root destroy -auto-approve"
+  retry 3 "terraform -chdir=$SCRIPTDIR/$ROOT destroy -auto-approve"
   INFO "Destroy the rest completed."
-  rm -f "$SCRIPTDIR/$root/terraform.output"
+  rm -f "$SCRIPTDIR/$ROOT/terraform.output"
 }
 
 tf-destroy-wl () {
-  local root="$1"
-  export TF_LOG_PATH="$SCRIPTDIR/$root/terraform.log"
-  retry 3 "terraform -chdir=$SCRIPTDIR/$root destroy -target=module.eks_blueprints_addon_cbci -auto-approve"
+  export TF_LOG_PATH="$SCRIPTDIR/$ROOT/terraform.log"
+  retry 3 "terraform -chdir=$SCRIPTDIR/$ROOT destroy -target=module.eks_blueprints_addon_cbci -auto-approve"
   INFO "Destroy target module.eks_blueprints_addon_cbci completed."
-  retry 3 "terraform -chdir=$SCRIPTDIR/$root destroy -target=module.eks_blueprints_addons -auto-approve"
+  retry 3 "terraform -chdir=$SCRIPTDIR/$ROOT destroy -target=module.eks_blueprints_addons -auto-approve"
   INFO "Destroy target module.eks_blueprints_addons completed."
 }
 
 probes () {
-  local root="$1"
   local wait=5
-  eval "$(tf-output "$root" kubeconfig_export)"
-  until [ "$(eval "$(tf-output "$root" cbci_oc_pod)" | awk '{ print $3 }' | grep -v STATUS | grep -v -c Running)" == 0 ]; do sleep 10 && echo "Waiting for Operation Center Pod to get ready..."; done ;\
-    eval "$(tf-output "$root" cbci_oc_pod)" && INFO "OC Pod is Ready."
-  until eval "$(tf-output "$root" cbci_liveness_probe_int)"; do sleep $wait && echo "Waiting for Operation Center Service to pass Health Check from inside the cluster..."; done
+  eval "$(tf-output kubeconfig_export)"
+  until [ "$(eval "$(tf-output cbci_oc_pod)" | awk '{ print $3 }' | grep -v STATUS | grep -v -c Running)" == 0 ]; do sleep 10 && echo "Waiting for Operation Center Pod to get ready..."; done ;\
+    eval "$(tf-output cbci_oc_pod)" && INFO "OC Pod is Ready."
+  until eval "$(tf-output cbci_liveness_probe_int)"; do sleep $wait && echo "Waiting for Operation Center Service to pass Health Check from inside the cluster..."; done
     INFO "Operation Center Service passed Health Check inside the cluster." ;\
-  until eval "$(tf-output "$root" cbci_oc_ing)"; do sleep $wait && echo "Waiting for Operation Center Ingress to get ready..."; done ;\
+  until eval "$(tf-output cbci_oc_ing)"; do sleep $wait && echo "Waiting for Operation Center Ingress to get ready..."; done ;\
     INFO "Operation Center Ingress Ready."
-  OC_URL=$(tf-output "$root" cbci_oc_url)
-  until eval "$(tf-output "$root" cbci_liveness_probe_ext)"; do sleep $wait && echo "Waiting for Operation Center Service to pass Health Check from outside the clustery..."; done ;\
+  OC_URL=$(tf-output cbci_oc_url)
+  until eval "$(tf-output cbci_liveness_probe_ext)"; do sleep $wait && echo "Waiting for Operation Center Service to pass Health Check from outside the cluster..."; done ;\
     INFO "Operation Center Service passed Health Check outside the cluster. It is available at $OC_URL."
-  if [ "$root" == "${BLUEPRINTS[0]}" ] ; then
-    INITIAL_PASS=$(eval "$(tf-output "$root" cbci_initial_admin_password)"); \
+  if [ "$ROOT" == "${BLUEPRINTS[0]}" ] ; then
+    INITIAL_PASS=$(eval "$(tf-output cbci_initial_admin_password)"); \
       INFO "Initial Admin Password: $INITIAL_PASS."
   fi
-  if [ "$root" == "${BLUEPRINTS[1]}" ]; then
-    GLOBAL_PASS=$(eval "$(tf-output "$root" global_password)") && \
+  if [ "$ROOT" == "${BLUEPRINTS[1]}" ]; then
+    GLOBAL_PASS=$(eval "$(tf-output global_password)") && \
       if [ -n "$GLOBAL_PASS" ]; then
         INFO "Password for admin_cbci_a: $GLOBAL_PASS."
       else
         ERROR "Problem while getting Global Pass."
       fi
-    until { eval "$(tf-output "$root" cbci_oc_export_admin_crumb)" && eval "$(tf-output "$root" cbci_oc_export_admin_api_token)" && [ -n "$CBCI_ADMIN_TOKEN" ]; }; do sleep $wait && echo "Waiting for Admin Token..."; done && INFO "Admin Token: $CBCI_ADMIN_TOKEN"
-    eval "$(tf-output "$root" cbci_controller_b_s3_build)" > /tmp/controller-b-hibernation &&
+    until { eval "$(tf-output cbci_oc_export_admin_crumb)" && eval "$(tf-output cbci_oc_export_admin_api_token)" && [ -n "$CBCI_ADMIN_TOKEN" ]; }; do sleep $wait && echo "Waiting for Admin Token..."; done && INFO "Admin Token: $CBCI_ADMIN_TOKEN"
+    eval "$(tf-output cbci_controller_b_s3_build)" > /tmp/controller-b-hibernation &&
       if grep "201\|202" /tmp/controller-b-hibernation; then
         INFO "Hibernation Post Queue Controller B OK."
       else
         ERROR "Hibernation Post Queue Controller B KO."
       fi
-    eval "$(tf-output "$root" cbci_controller_c_windows_node_build)" > /tmp/controller-c-hibernation &&
+    eval "$(tf-output cbci_controller_c_windows_node_build)" > /tmp/controller-c-hibernation &&
       if grep "201\|202" /tmp/controller-c-hibernation; then
         INFO "Hibernation Post Queue Controller C OK."
       else
         ERROR "Hibernation Post Queue Controller C KO."
       fi
-    until eval "$(tf-output "$root" cbci_controller_c_hpa)"; do sleep $wait && echo "Waiting for Team C HPA to get Ready..."; done ;\
+    until eval "$(tf-output cbci_controller_c_hpa)"; do sleep $wait && echo "Waiting for Team C HPA to get Ready..."; done ;\
       INFO "Team C HPA is Ready."
-    until [ "$(eval "$(tf-output "$root" cbci_controllers_pods)" | awk '{ print $3 }' | grep -v STATUS | grep -v -c Running)" == 0 ]; do sleep $wait && echo "Waiting for Controllers Pod to get into Ready State..."; done ;\
-      eval "$(tf-output "$root" cbci_controllers_pods)" && INFO "All Controllers Pods are Ready."
-    until [ "$(eval "$(tf-output "$root" cbci_agent_windowstempl_events)" | grep -c 'Allocated Resource vpc.amazonaws.com')" -ge 1 ]; do sleep $wait && echo "Waiting for Windows Template Pod to allocate resource vpc.amazonaws.com"; done ;\
-      eval "$(tf-output "$root" cbci_agent_windowstempl_events)" && INFO "Windows Template Example is OK."
-    until [ "$(eval "$(tf-output "$root" cbci_agent_linuxtempl_events)" | grep -c 'Created container: maven')" -ge 2 ]; do sleep $wait && echo "Waiting for both Linux Template Pods to create maven container"; done ;\
-      eval "$(tf-output "$root" cbci_agent_linuxtempl_events)" && INFO "Linux Template Example is OK."
-    until [ "$(eval "$(tf-output "$root" s3_list_objects)" | grep -c 'cbci/')" -ge 2 ]; do sleep $wait && echo "Waiting for WS Cache and Artifacts to be uploaded into s3 cbci"; done ;\
-      eval "$(tf-output "$root" s3_list_objects)" | grep 'cbci/' && INFO "CBCI s3 Permissions are configured correctly."
-    eval "$(tf-output "$root" velero_backup_schedule)" && eval "$(tf-output "$root" velero_backup_on_demand)" > /tmp/velero-backup.txt && \
+    until [ "$(eval "$(tf-output cbci_controllers_pods)" | awk '{ print $3 }' | grep -v STATUS | grep -v -c Running)" == 0 ]; do sleep $wait && echo "Waiting for Controllers Pod to get into Ready State..."; done ;\
+      eval "$(tf-output cbci_controllers_pods)" && INFO "All Controllers Pods are Ready."
+    until [ "$(eval "$(tf-output cbci_agent_windowstempl_events)" | grep -c 'Allocated Resource vpc.amazonaws.com')" -ge 1 ]; do sleep $wait && echo "Waiting for Windows Template Pod to allocate resource vpc.amazonaws.com"; done ;\
+      eval "$(tf-output cbci_agent_windowstempl_events)" && INFO "Windows Template Example is OK."
+    until [ "$(eval "$(tf-output cbci_agent_linuxtempl_events)" | grep -c 'Created container: maven')" -ge 2 ]; do sleep $wait && echo "Waiting for both Linux Template Pods to create maven container"; done ;\
+      eval "$(tf-output cbci_agent_linuxtempl_events)" && INFO "Linux Template Example is OK."
+    until [ "$(eval "$(tf-output s3_list_objects)" | grep -c 'cbci/')" -ge 2 ]; do sleep $wait && echo "Waiting for WS Cache and Artifacts to be uploaded into s3 cbci"; done ;\
+      eval "$(tf-output s3_list_objects)" | grep 'cbci/' && INFO "CBCI s3 Permissions are configured correctly."
+    eval "$(tf-output velero_backup_schedule)" && eval "$(tf-output velero_backup_on_demand)" > /tmp/velero-backup.txt && \
       if grep 'Backup completed with status: Completed' /tmp/velero-backup.txt; then
         INFO "Velero Backups are OK."
       else
         ERROR "Velero Backups are K0."
       fi
-    until eval "$(tf-output "$root" prometheus_active_targets)" | jq '.data.activeTargets[] | select(.labels.container=="jenkins") | {job: .labels.job, instance: .labels.instance, status: .health}'; do sleep $wait && echo "Waiting for CloudBees CI Prometheus Targets..."; done ;\
+    until eval "$(tf-output prometheus_active_targets)" | jq '.data.activeTargets[] | select(.labels.container=="jenkins") | {job: .labels.job, instance: .labels.instance, status: .health}'; do sleep $wait && echo "Waiting for CloudBees CI Prometheus Targets..."; done ;\
       INFO "CloudBees CI Targets are loaded in Prometheus."
-    until [ "$(eval "$(tf-output "$root" tempo_tags)" | grep -c 'jenkins.pipeline')" -ge 1 ]; do sleep $wait && echo "Waiting for Tempo to inject jenkins.pipeline* tags from Open Telemetry plugin"; done ;\
-      eval "$(tf-output "$root" tempo_tags)" | jq .tagNames && INFO "Tempo has injested tags from Open Telemetry plugin."
-    until [ "$(eval "$(tf-output "$root" loki_labels)" | grep -c 'com_cloudbees')" -ge 1 ]; do sleep $wait && echo "Waiting for Loki to inject com_cloudbees* labels from FluentBit"; done ;\
-      eval "$(tf-output "$root" loki_label)" && INFO "Loki has injested labels from FluentBit."
+    until [ "$(eval "$(tf-output tempo_tags)" | grep -c 'jenkins.pipeline')" -ge 1 ]; do sleep $wait && echo "Waiting for Tempo to inject jenkins.pipeline* tags from Open Telemetry plugin"; done ;\
+      eval "$(tf-output tempo_tags)" | jq .tagNames && INFO "Tempo has injested tags from Open Telemetry plugin."
+    until [ "$(eval "$(tf-output loki_labels)" | grep -c 'com_cloudbees')" -ge 1 ]; do sleep $wait && echo "Waiting for Loki to inject com_cloudbees* labels from FluentBit"; done ;\
+      eval "$(tf-output loki_label)" && INFO "Loki has injested labels from FluentBit."
     # Note: name aws fluent bit  log streams is not consistent, it has a random suffix
-    # until eval "$(tf-output "$root" aws_logstreams_fluentbit)" | jq '.[] '; do sleep $wait && echo "Waiting for CloudBees CI Log streams in CloudWatch..."; done ;\
+    # until eval "$(tf-output aws_logstreams_fluentbit)" | jq '.[] '; do sleep $wait && echo "Waiting for CloudBees CI Log streams in CloudWatch..."; done ;\
     #   INFO "CloudBees CI Log Streams are already in Cloud Watch."
   fi
 }
